86fc27dfa2a9c5aacdf293b4b58e41a39b547b0c
Elliot Wesoff <elliot@techrockstars.com>
2017-07-14 01:52:28 +0200

    wip

---
+++ options_parser.rb
@@ -0,0 +1,52 @@
+require 'ostruct'
+
+module OptionsParser
+
+  def parse_options
+    options = OpenStruct.new
+    ARGV.unshift(' ')
+    cli_args = ARGV.join(' ')
+    matcher = -> (switch) { /(?<=\s\-#{switch})\s?(\S+)(?=\s\-)?/ }
+    s = cli_args.match(matcher.call('s'))
+    d = cli_args.match(matcher.call('d'))
+    f = cli_args.match(matcher.call('f'))
+    b = cli_args.match(matcher.call('b'))
+    t = cli_args.match(matcher.call('t'))
+    v = cli_args.match(matcher.call('v'))
+    h = cli_args.match(matcher.call('h'))
+    g = cli_args.match(matcher.call('g'))
+    if s && d && !h
+      options.source = s.captures.first
+      options.destination = d.captures.first
+      options.omit_filetypes = f ? f.captures.first.split(";") : []
+      options.minimum_size = b ? b.captures.first : 0
+      options.test_run = t ? true : false
+      options.verbose = v ? true : false
+      options.debug = g ? true : false
+    else
+      puts menu_text
+      exit 0
+    end
+    return options
+  end
+
+  def menu_text
+    str = <<-MENU
+    ruby picture-mover.rb -s [source] -d [destination] [ARGS]
+    
+    Available switches:
+    -s : source directory *
+    -d : destination directory *
+    -f : omit-filetypes, semicolon delimited
+    -b : omit files by size (in bytes)
+    -t : test run - don't actually copy any files
+    -v : verbose - say all the things
+    -h : help - show this message
+
+    * indicates required switch
+    Copyright Elliot Wesoff, #{Time.now.year}
+    MENU
+    return str.split("\n").map(&:strip).join("\n")
+  end
+
+end
--- picture-mover.rb
+++ picture-mover.rb
@@ -1,60 +1,61 @@
+#!/usr/bin/env ruby
+
 require 'rubygems'
 require 'fileutils'
 require 'pry'
 
-require './questionnaire'
+require './options_parser'
 require './media_folder'
 require './media_item'
 require './image_db'
 
+ARGS = ARGV
 
 class PictureMover
 
-  attr_accessor :written, :duplicate, :thumbnail, :start, :finish, :questionnaire, :media_folder, :image_db, :media
+  attr_accessor :options, :written, :duplicate, :thumbnail
+
+  include OptionsParser
   
   def initialize
-    @questionnaire = Questionnaire.new
-    @written   = 0
-    @duplicate = 0
-    @thumbnail = 0
+    @options = parse_options
+  end
+
+  def display_stats
+  end
+
+  def get_approval
+    puts "Press enter to continue..."
+    STDIN.getc
   end
 
   def execute
-    @questionnaire.ask_questionnaire
-    @image_db = ImageDB.new(q.dest_dir.to_s)
-    @media_folder = MediaFolder.new
-    @media = @media_folder.load_media(q.source_dir)
-    print "\nPress enter to begin... "; gets
-    @start = Time.now
-    @media.each do |file_name|
+    image_db = ImageDB.new(options.destination)
+    media_folder = MediaFolder.new
+    media = media_folder.load_media(options.source)
+    written, duplicate, thumbnail = 0, 0, 0
+    binding.pry if options.debug
+    display_stats
+    get_approval
+    start = Time.now
+    media.each do |file_name|
       file = File.open(file_name, 'r')
       sha = Digest::SHA256.hexdigest(file.read)
 
       # if the sha is not in the dictionary, merge it into the destination folder.
       # if it is, consider it a duplicate and skip it.
       # http://stackoverflow.com/questions/9333952/case-insensitive-arrayinclude
-      unless @media_folder.sha_dictionary.any? { |s| s.casecmp(sha) == 0 }
+      unless media_folder.sha_dictionary.any? { |s| s.casecmp(sha) == 0 }
 
         # if the filename already exists in the folder, give it a new name so the old one isn't overwritten.
-        if @media_folder.file_dictionary.any? { |f| f.casecmp(File.basename(file_name)) == 0 }
+        if media_folder.file_dictionary.any? { |f| f.casecmp(File.basename(file_name)) == 0 }
 
-          existing_file = File.open("#{q.dest_dir}/#{File.basename(file_name)}")
+          existing_file = File.open("#{options.destination}/#{File.basename(file_name)}")
 
-          # if file is less than 100K, and the user chose to skip thumbs, consider it a thumb and skip it.
-          if file.size < 100000
-            puts "skipped thumbnail: #{file_name}"
-            @thumbnail += 1
+          if file.size < options.minimum_size
+            puts "file doesn't meet size threshold of #(options.minimum_size}B: #{file_name}" if options.verbose
+            thumbnail += 1
             next 
-          end
-
-          # if the sha isn't in the dictionary, and the file name is already taken in the destination,
-          # and the size differences between the original and the new file are 0B or 100K, then it's
-          # safe to assume the two files are the same, skip it.
-          diff = (file.size - existing_file.size).abs
-          if diff == 0 || diff < 100000
-            puts "skipped duplicate: #{file_name}"
-            @duplicate += 1
-            next
           end
 
           # if the existing file is less than 100KB and the new one is larger,
@@ -63,33 +64,36 @@ class PictureMover
           dest = if existing_file.size < 100000 && file.size > 100000
             existing_file
           else
-            puts "generating new file name for conflicting file: #{existing_file.path}"
-            unique_name = "#{q.dest_dir}/#{File.basename(file_name, ".*")}-#{sha}#{File.extname(file)}"
+            puts "generating new file name for conflicting file: #{existing_file.path}" if options.verbose
+            unique_name = "#{options.destination}/#{File.basename(file_name, ".*")}-#{sha}#{File.extname(file)}"
             puts unique_name
             unique_name
           end
 
         else
           basename = File.basename(file_name)
-          dest = "#{q.dest_dir}/#{basename}"
+          dest = "#{options.destination}/#{basename}"
         end
 
-        FileUtils.cp(file_name, dest, preserve: true)
+        FileUtils.cp(file_name, dest, preserve: true, noop: options.test_run)
         MediaItem.persist_item("#{dest}/#{file_name}", sha)
         puts "copied #{file_name} -> #{dest}"
-        @written += 1
+        written += 1
 
       else # matches the unless.
         puts "skipped duplicate: #{file_name}"
-        @duplicate += 1
+        duplicate += 1
       end
     end
 
-    @finish = Time.now
-    
+    finish = Time.now
+
     puts "\nDone!"
-    puts "Processed #{@media.count} files in #{(@finish - @start).round(3)} seconds."
-    puts "Wrote #{@written} new files."
-    puts "Skipped #{@duplicate} duplicates, skipped #{@thumbnail} thumbnails."
+    puts "Processed #{media.count} files in #{(finish - start).round(3)} seconds."
+    puts "Wrote #{written} new files."
+    puts "Skipped #{duplicate} duplicates, skipped #{thumbnail} thumbnails."
   end
+
 end
+
+PictureMover.new.execute
