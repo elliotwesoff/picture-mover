2e3703c173bcb44d4337ae0e33a4101fa5d7bceb
Elliot Wesoff <elliot@techrockstars.com>
2017-08-26 12:19:14 +0200

    add source_duplicates test data folder and data.

---
+++ test/source_duplicates/readme.txt
@@ -0,0 +1,3 @@
+the source folder in this directory should contain files (and files in subdirectories) that share the same name. they should be renamed before the start of the copy process.
+
+the images in ./source/2 are unique images to those in ./source/1, but two of the three share the same name as photos in ./source/1. all photos sharing the same name shall be renamed.
---
+++ test/spec.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'test/unit'
+require '../picture_mover'
+require '../media_folder'
+require '../questionnaire'
+require '../user_input'
+require '../media_item'
+require '../image_db'
+ 
+ 
+class TestPictureMover < Test::Unit::TestCase
+end
+
+class TestMediaItem < Test::Unit::TestCase
+end
+ 
+class TestImageDb < Test::Unit::TestCase
+end
+ 
+class TestQuestionnaire < Test::Unit::TestCase
+end
+ 
+class TestUserInput < Test::Unit::TestCase
+end
+ 
+class TestMediaFolder < Test::Unit::TestCase
+end
